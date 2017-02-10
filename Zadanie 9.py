47d70dec56becf0042bcbba54d523f407f1f347b
Артур Коновалов <ar4iinfo@gmail.com>
2017-02-10 19:55:36 +0300

    Delete Zadanie 9.py

--- Zadanie 9.py
+++
@@ -1,9 +0,0 @@
-#На концерте певицы было А % отдыхающих в санатории, на выступление хора зрителей собралось в 1,5 раза больше. Сколько зрителей побывало на этих концертах в общем, если всего в санатории отдыхало х человек.
-
-lydVSanatorii = int(input('Введите кол-во людей в санатории: '))
-procent = int(input('Введите процент людей посетивших концерт: '))
-lydNaSvistoplaske = (lydVSanatorii * procent)/100
-lydNaHorePravoslavnom = lydNaSvistoplaske * 1.5
-obshiiLydNaHorePravoslavnomISvistoplaske = lydNaHorePravoslavnom + lydNaSvistoplaske
-print('Общее кол-во людей поситившие мероприятия %s' % obshiiLydNaHorePravoslavnomISvistoplaske)
-input('Нажми Ввод для продолжения...')
